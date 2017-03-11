3eac205f2b9ea8c4d4be750d9782a226ba6b83e9
Alexandro <alexandro1418@gmail.com>
2017-03-11 09:15:24 +0100

    test con reactJS

---
+++ src/client/testReact.jsx
@@ -0,0 +1,15 @@
+import React freom 'react'
+
+class MyComponent extends React.Component {
+	constructor (){
+		super()
+	}
+
+	render (){
+		return (
+			<div>
+				<span>Hola, soy un componente</span>
+			</div>
+		)
+	}
+}
